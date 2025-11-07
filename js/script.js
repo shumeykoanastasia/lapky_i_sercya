4aa285052fb227c2e03039bc154ad5814440747a
shumeykoanastasia <shumeyko.anastasia@lll.kpi.ua>
2025-11-07 19:36:16 +0200

    add css for all pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//шматочок коду для роботи бургер-меню
+const hamburger_menu = document.getElementById('hamburger-menu')
+const nav_menu = document.getElementById('nav-menu')
+//додаємо обробку подій - при кліці на кнопку
+//до елементу hamburger-menu додається або прибирається класс active
+hamburger_menu.addEventListener('click',()=>{
+    hamburger_menu.classList.toggle('active');
+    nav_menu.classList.toggle('active') ;
+})
